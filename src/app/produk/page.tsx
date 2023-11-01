f9a3c1835a8a4d6e0d02c1e04c90717edf3bc72a
Raphaeline <zlfkhr.akbar@gmail.com>
2023-11-01 15:52:58 +0700

    third commit

--- src/app/produk/page.tsx
+++ src/app/produk/page.tsx
@@ -1,4 +1,5 @@
-// import React, { useState } from "react";
+"use client";
+import React, { useState } from "react";
 import Navbar from "../../../components/Navbar";
 // const Navbar = ({ isDashboardPage, isProdukPage, isAdminPage }: { isDashboardPage: boolean; isProdukPage: boolean; isAdminPage: boolean }) => {
 //   return (
@@ -41,27 +42,26 @@ import Navbar from "../../../components/Navbar";
 //     </nav>
 //   );
 // };
-
 const Produk = () => {
-  // const [allChecked, setAllChecked] = useState(false);
-  // const [individualChecks, setIndividualChecks] = useState([false, false]);
+  const [allChecked, setAllChecked] = useState(false);
+  const [individualChecks, setIndividualChecks] = useState([false, false]);
 
-  // const handleMainCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setAllChecked(e.target.checked);
-  //   setIndividualChecks(individualChecks.map(() => e.target.checked));
-  // };
+  const handleMainCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAllChecked(e.target.checked);
+    setIndividualChecks(individualChecks.map(() => e.target.checked));
+  };
 
-  // const handleIndividualCheckbox = (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const updatedChecks = [...individualChecks];
-  //   updatedChecks[index] = e.target.checked;
-  //   setIndividualChecks(updatedChecks);
+  const handleIndividualCheckbox = (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedChecks = [...individualChecks];
+    updatedChecks[index] = e.target.checked;
+    setIndividualChecks(updatedChecks);
 
-  //   setAllChecked(updatedChecks.every((check) => check));
-  // };
+    setAllChecked(updatedChecks.every((check) => check));
+  };
 
   return (
     <main className="min-h-screen flex-col items-center p-24 bg-[#f4f9fb]">
-      <Navbar isDashboardPage={true} isProdukPage={false} isAdminPage={false} />
+      <Navbar isDashboardPage={false} isProdukPage={true} isAdminPage={false} />
       <div className="fixed right-10 top-10">
         <a href="#" className="bg-[#55A3BD] hover:bg-[#5495a2] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md shadow-md mr-4">
           Buat Kategori
@@ -70,15 +70,15 @@ const Produk = () => {
           Tambah Produk
         </a>
       </div>
-      <div className="relative left-28 mx-10">
+      <div className="fixed left-64 mx-10">
         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
           <img src="/produk/searchIcon.svg" alt="Search Icon" className="w-4 h-4" />
         </span>
-        <input type="text" id="searchInput" placeholder="Cari produk (nama/UPC/SKU)" className="font-poppins font-[18px] text-[#98A2A5] w-[310px] 2xl:w-[583px] h-[32px] 2xl:h-[48px] pl-10 rounded border border-[#98A2A5]" />
+        <input type="text" id="searchInput" placeholder="Cari produk (nama/UPC/SKU)" className="font-poppins font-[15px] text-[#98A2A5] w-[270px] 2xl:w-[583px] h-[32px] 2xl:h-[48px] pl-10 rounded border border-[#98A2A5]" />
       </div>
 
-      <div className="relative left-28 mx-10 mt-5">
-        {/* <table>
+      <div className="fixed left-64 mx-10 mt-16">
+        <table>
           <thead className="bg-[#E4F2F6]">
             <tr>
               <th className="border border-[#D4E3E8] py-[15px] px-[20px]">
@@ -109,7 +109,7 @@ const Produk = () => {
               </tr>
             ))}
           </tbody>
-        </table> */}
+        </table>
       </div>
     </main>
   );
